ef342cc69c67fc38ab8c0ea0fc15aa6974ec6762
Rajdeep <rajdeepxchintan@gmail.com>
2026-01-27 01:44:56 +0530

    Base Commit

--- src/GFSClient.java
+++ src/GFSClient.java
@@ -1,6 +1,7 @@
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.List;
 
 public class GFSClient {
 
@@ -12,15 +13,27 @@ public class GFSClient {
             ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
 
             Message msg = new Message();
-            msg.type = RequestType.WRITE_CHUNK;
-            msg.fileName = "chunk_1";
+            msg.type = RequestType.GET_CHUNKS;
+            msg.fileName = "test.txt";
             msg.data = "Hello GFS".getBytes();
 
             out.writeObject(msg);
             out.flush();
 
-            Object response = in.readObject();
-            System.out.println("Response from master: " + response);
+            Object obj = in.readObject();
+
+            if (!(obj instanceof Message)) {
+                throw new RuntimeException(
+                        "Protocol violation: expected Message, got " + obj.getClass()
+                );
+            }
+
+            Message response = (Message) obj;
+
+            System.out.println("Response type: " + response.type);
+            System.out.println("File: " + response.fileName);
+            System.out.println("Chunks: " + response.chunkList);
+
 
             socket.close();
         } catch (Exception e) {
--- src/Message.java
+++ src/Message.java
@@ -1,7 +1,14 @@
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
 public class Message implements Serializable {
     public RequestType type;
     public String fileName;
+    public List<String> chunkList;
     public byte[] data;
+
+    // Response fields
+    public List<String> chunks;
+    public Map<String, List<String>> chunkLocations;
 }
